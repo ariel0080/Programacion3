c812e6e0fe7bc22ddb55139dc103d4356bb6ab23
luloop <lucilarizzi@gmail.com>
2019-05-13 17:28:36 -0300

    garage en proceso

--- clase02/Proy2/auto.php
+++ clase02/Proy2/auto.php
@@ -19,7 +19,7 @@ class Auto
 
     public function AgregarImpuestos($sumarAlPrecio)
     {
-        $this->precio += $sumarAlPrecio;
+        $this->precio += (int) $sumarAlPrecio;
     }
 
     public function MostrarAuto()
@@ -29,7 +29,7 @@ class Auto
         "<br> Precio: " . $this->precio . "<br> Fecha: " . $this->fecha;
     }
 
-    public function Equals($auto1, $auto2)
+    public static function Equals($auto1, $auto2)
     {
         if ($auto1->marca == $auto2->marca) {
             return true;
@@ -38,11 +38,9 @@ class Auto
 
     public static function Add($auto1, $autoAgregar)
     {
-        if($auto1->marca == $autoAgregar->marca && $auto1->color == $autoAgregar->color)
-        {
+        if ($auto1->marca == $autoAgregar->marca && $auto1->color == $autoAgregar->color) {
             return $auto1->precio + $autoAgregar->precio;
-        }
-        else{
+        } else {
             echo "LOS AUTOS NO SON IGUALES";
             return 0;
         }
---
+++ clase02/Proy2/testAuto.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'auto.php';
+
+$autoIgual1 = new Auto('Negro', '1000', 'Fiat', '2015');
+$autoIgual2 = new Auto('Negro', '1000', 'Fiat', '2015');
+
+$autoIgual3 = new Auto('Negro', '2000', 'Fiat', '2015');
+$autoIgual4 = new Auto('Negro', '3000', 'Fiat', '2015');
+
+// Utilizar el método “AgregarImpuesto” en los últimos tres objetos,
+// agregando $ 1500 al atributo precio.
+
+$autoIgual2->AgregarImpuesto('1500');
+$autoIgual3->AgregarImpuesto('1500');
+$autoIgual4->AgregarImpuesto('1500');
+
+echo (Auto::Add($autoIgual1, $autoIgual2));
+
+
+?>

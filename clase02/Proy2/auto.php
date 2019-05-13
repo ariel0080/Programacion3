17571ec3459fe7d8239540334c181571f2e6e652
luloop <lucilarizzi@gmail.com>
2019-05-13 17:08:55 -0300

    mas mas

--- clase02/Proy2/auto.php
+++ clase02/Proy2/auto.php
@@ -17,17 +17,37 @@ class Auto
 
     }
 
-
-    function static AgregarImpuestos ($sumarAlPrecio)
+    public function AgregarImpuestos($sumarAlPrecio)
     {
         $this->precio += $sumarAlPrecio;
     }
-    function  MostrarAuto()
+
+    public function MostrarAuto()
     {
         echo "<font size='3' color='blue'  face='verdana' style='font-weight:bold'>
-        Auto </font>  Marca: ".$this->marca."<br> Color: ".$this->color.
-        "<br> Precio: ".$this->precio."<br> Fecha: ".$this->fecha;
+        Auto </font>  Marca: " . $this->marca . "<br> Color: " . $this->color .
+        "<br> Precio: " . $this->precio . "<br> Fecha: " . $this->fecha;
     }
+
+    public function Equals($auto1, $auto2)
+    {
+        if ($auto1->marca == $auto2->marca) {
+            return true;
+        }
+    }
+
+    public static function Add($auto1, $autoAgregar)
+    {
+        if($auto1->marca == $autoAgregar->marca && $auto1->color == $autoAgregar->color)
+        {
+            return $auto1->precio + $autoAgregar->precio;
+        }
+        else{
+            echo "LOS AUTOS NO SON IGUALES";
+            return 0;
+        }
+    }
+
 }
 
 ?>

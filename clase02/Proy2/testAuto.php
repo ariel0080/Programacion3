5ff05336b05af3265e3075d8b8f38eb0333ed7fb
luloop <lucilarizzi@gmail.com>
2019-05-13 17:53:28 -0300

    mas

--- clase02/Proy2/testAuto.php
+++ clase02/Proy2/testAuto.php
@@ -11,9 +11,11 @@ $autoIgual4 = new Auto('Negro', '3000', 'Fiat', '2015');
 // Utilizar el método “AgregarImpuesto” en los últimos tres objetos,
 // agregando $ 1500 al atributo precio.
 
+$autoIgual3->MostrarAuto();
+
 $autoIgual2->AgregarImpuesto('1500');
-$autoIgual3->AgregarImpuesto('1500');
-$autoIgual4->AgregarImpuesto('1500');
+//$autoIgual3->AgregarImpuesto('1500');
+//$autoIgual4->AgregarImpuesto('1500');
 
 echo (Auto::Add($autoIgual1, $autoIgual2));
 

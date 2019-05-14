329ff108b9c3e474a9c9cc651ac32698b6187ee8
luloop <lucilarizzi@gmail.com>
2019-05-14 13:03:57 -0300

    sss

--- clase02/Proy2/Garage.php
+++ clase02/Proy2/Garage.php
@@ -4,71 +4,55 @@ require 'auto.php';
 
 class Garage
 {
-private $precioPorHora;
-private $autos;
-private $razonSocial;
+    private $precioPorHora;
+    private $autos;
+    private $razonSocial;
 
-public function __construct($razon, $precio)
-{
-    $this->razonSocial= $razon;
-    $this->precioPorHora = $precio;
-    $this->autos = array();
-}
+    public function __construct($razon, $precio)
+    {
+        $this->razonSocial = $razon;
+        $this->precioPorHora = $precio;
+        $this->autos = array();
+    }
 
-public function MostrarGarage()
-{
-
+    public function MostrarGarage()
+    {
         echo "<font size='3' color='blue'  face='verdana' style='font-weight:bold'>
-        <br> Garage <br> </font> Razon Social: " . $this->razonSocial.
-        "<br> Precio: ". $this->precioPorHora;
-        foreach($this->autos as $auto)
-        {
+        <br> Garage <br> </font> Razon Social: " . $this->razonSocial .
+        "<br> Precio: " . $this->precioPorHora;
+        foreach ($this->autos as $auto) {
             $auto->MostrarAuto();
         }
-
-}
-
-public function Equals ($auto)
-{
-   return in_array($this->autos, $auto);
-
-}
-
-public function Add($autoAgregar)
-{
-    if (in_array($this->autos, $autoAgregar))
-    {
-        echo "<br>----------------el auto ya se encuenta en el garage!<br>----------------";
-
-    } else {
-        $this->autos->array_push($autoAgregar);
     }
-}
 
-public function Remove ($autoQuita)
-{
-$esta=false;
-
-    foreach( $this->autos as $auto)
+    public function Equals($auto)
     {
-        if($auto->Equals($autoQuita))
-        {
-            unset($this->autos, $auto)
-            $esta = true;
+        return in_array($this->autos, $auto);
+    }
+
+    public function Add($autoAgregar)
+    {
+        if (in_array($this->autos, $autoAgregar)) {
+            echo "<br>----------------el auto ya se encuenta en el garage!<br>----------------";
+        } else {
+            $this->autos->array_push($autoAgregar);
         }
     }
-    if(!$esta)
+
+    public function Remove($autoQuita)
     {
-        echo "<br>----------------el auto no se encuentra en el garage<br>----------------";
+        $esta = false;
 
+        foreach ($this->autos as $auto) {
+            if ($auto->Equals($autoQuita)) {
+                unset($this->autos, $auto);
+                $esta = true;
+            }
+        }
+        if (!$esta) {
+            echo "<br>----------------el auto no se encuentra en el garage<br>----------------";
+        }
     }
-
-
 }
-
-
-
-
-
 
 ?>

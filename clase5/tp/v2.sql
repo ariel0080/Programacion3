f484085b5c51b7da53cb3abf389b42041afc11b7
luloop <E-Mail del usuario del GitHub>
2019-06-04 14:45:31 -0300

    mysql quiero llorar

--- clase5/tp/v2.sql
+++ clase5/tp/v2.sql
@@ -16,3 +16,67 @@ VALUES("Caramelos", 1.5, "Chico"),("Cigarrillos", 45.89, "Mediano"),("Gaseosa", 
 SELECT * FROM productos ORDER BY pNombre
 
 2.
+SELECT * FROM `provedores​` WHERE localidad='Quilmes'
+
+3.
+SELECT * FROM `envios` WHERE cantidad BETWEEN 200 AND 300
+
+4.
+SELECT SUM(cantidad) AS 'cantidad total de productos enviados' FROM envios a
+
+5.
+SELECT * FROM envios a ORDER BY a.pNumero LIMIT 3
+
+6. // no funciono //
+SELECT
+    provedores.nombre,
+    producto.pNombre
+FROM
+    provedores provedor,
+    productos producto,
+    envios e
+WHERE
+    provedor.numero = e.numero AND producto.pNumero = e.pNumero;
+
+///////////////
+
+7.
+SELECT
+    provee.*,
+    produc.*,
+    envi.cantidad,
+    (cantidad * precio) AS 'Monto de envios'
+FROM
+    provedores AS provee,
+    productos AS produc,
+    envios AS envi
+WHERE
+    provee.numero = envi.numero AND produc.pNumero = envi.pNumero
+
+8.
+SELECT SUM(cantidad) 'Cantidad total' FROM envios env WHERE env.pNumero = 1 AND env.numero = 102
+
+9.
+SELECT produc.pNumero FROM provedores proveed, productos produc, envios envi WHERE produc.pNumero = envi.pNumero AND proveed.numero = envi.numero AND proveed.localidad = 'Avellaneda'
+
+10.
+SELECT provedores.domicilio, provedores.localidad FROM provedores WHERE nombre LIKE '%i%'
+
+11.
+INSERT INTO productos(pNombre, precio, tamaño)
+VALUES('Chocolate', '25.35', 'Chico')
+
+12.
+INSERT INTO provedores() VALUES()
+
+13.
+INSERT INTO provedores (numero, nombre, localidad) VALUES (107, 'Rosales', 'La Plata')
+
+14.
+UPDATE productos SET precio = 97.50 WHERE tamaño = 'grande'
+
+15.
+UPDATE productos SET tamaño = 'Mediano' WHERE tamaño = 'Chico' AND pNumero IN( SELECT DISTINCT productos.pNumero FROM envios envi WHERE productos.pNumero = envi.pNumero AND envi.cantidad >= 300 AND productos.tamaño = 'Chico' )
+
+16.
+DELETE FROM productos WHERE pNumero = 1
